8b7935d3a4a15f13959f8cfd8b0524fe671b9d59
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-07-07 18:22:34 -0400

    clean up

--- eatem-server/gameinterface.cpp
+++ eatem-server/gameinterface.cpp
@@ -1,26 +1,29 @@
 #include "gameinterface.h"
 
 
-GameInterface::GameInterface(QObject *parent) : QObject(parent)
+GameInterface::GameInterface(QObject *parent)
+    : QObject(parent)
+    , _websocket_server(new QWebSocketServer("Eatem", QWebSocketServer::NonSecureMode))
+    , _webchannel(new QWebChannel())
+    , _client_wrapper(new WebSocketClientWrapper(_websocket_server))
 {
+    // TODO: make configurable
+    bool listening = _websocket_server->listen(QHostAddress::LocalHost, 5555);
+
+    Q_UNUSED(listening)
+
     _game_size.setHeight(500);
     _game_size.setWidth(500);
-    // create our player
-    _this_player = new Player();
-    // Now we need to add our player to the list of players.
-    // Our list of players has a type of `QVariantList`
-    // So in order to add to this list, we have to create a new QVariant
-
-    // QVariant is a templated class. This means that we need
-    // to let the class know what our type is
-    _players.append(QVariant::fromValue<Player*>(_this_player));
-    // Note the syntax `QVariant::fromValue<Player*>`
-    // We're letting the templated function `fromValue` know
-    // That it'll be ingesting the type `Player*`
-    // Which is a pointer to our player instance.
 
     create_food();
     create_viruses();
+
+    _webchannel->registerObject("feed", _food);
+    _webchannel->registerObject("viruses", _viruses);
+    _webchannel->registerObject("players", _players);
+
+    connect(_client_wrapper, &WebSocketClientWrapper::clientConnected,
+            _webchannel, &QWebChannel::connectTo);
 }
 
 void GameInterface::create_viruses()
--- eatem-server/main.cpp
+++ eatem-server/main.cpp
@@ -12,31 +12,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QWebSocketServer *server = new QWebSocketServer("eatme", QWebSocketServer::SecureMode);
-    bool listening = server->listen(80);
-
-    Q_UNUSED(listening)
-
     // http://doc.qt.io/qt-5/qtwebchannel-chatclient-qml-qmlchatclient-qml.html
-    QObject::connect(server, &QWebSocketServer::newConnection, [](){
-        QWebSocket *client = server->nextPendingConnection();
-    });
-
-    // Get the root context from our QML engine
-    QQmlContext *root_context = engine.rootContext();
-
-
-    // ------------------------------------------
-    // FIXME: This code is not in the right place
-    // ------------------------------------------
-
-
-    // Step 1: get access to the root object
-    // NOTE: the root object is currently the ApplicationWindow
-    QObject *application_object = engine.rootObjects().first();
-    QObject *window_object = application_object->property("window").value<QObject*>();
-
-    // ------------------------------------------
 
     // Create our `GameInterface`, an interface which abstracts some of the game creation
     // aspects for us,  including:
@@ -45,19 +21,6 @@ int main(int argc, char *argv[])
     // 3. Creating a list of players
     GameInterface game_interface;
 
-    // We'll get out everything from the `game_interface` creates for us, and track
-    // it in the QML context using the `setContextProperty` method.
-    // The first argument in the method call is the name/accessor we'll use in QML
-    root_context->setContextProperty("viruses", game_interface.get_viruses());
-    root_context->setContextProperty("feed", game_interface.get_food());
-    root_context->setContextProperty("players", game_interface.get_players());
-
-    // NOTE: this method call  will go away when we set up the logic to create a player on arrival
-    root_context->setContextProperty("this_player", game_interface.get_this_player());
-
-    qDebug() << window_object;
-    QObject::connect(application_object, SIGNAL(height(int)), &game_interface, SLOT(set_game_height(int)));
-    QObject::connect(application_object, SIGNAL(width(int)), &game_interface, SLOT(set_game_width(int)));
 
     // We need to increment the game in a set amount of time.
     // We use a `QTimer` called `game_timestep` to trigger the game incremention
