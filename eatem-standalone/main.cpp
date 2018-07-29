2f31f248e30225388153b6dbc6df71dca62e98af
Ben Hoff <hoff.benjamin.k@gmail.com>
2018-07-29 19:57:48 -0400

    tracking standalone from separate dir

---
+++ eatem-standalone/main.cpp
@@ -0,0 +1,29 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "gameinterface.h"
+
+
+int main(int argc, char *argv[])
+{
+    // Enable High Dpi Scaling because ....
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    // Create our application, which controls our event loop
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<GameInterface>("GameInterfaces", 1, 0, "GameInterface");
+
+    // Create our QML application engine, which handles our QML
+    QQmlApplicationEngine engine;
+    // Load our `main.qml` page
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    // Check to see if we loaded the file correctly
+    if (engine.rootObjects().isEmpty())
+        // if we didn't load correctly, exit the main loop with the error/integer, `-1`
+        return -1;
+
+    return app.exec();
+}
